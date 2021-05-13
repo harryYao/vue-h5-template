9e10bfce23e65800da42a7cce3296f973dc9cc5e
yaoxin <yaoxin1@ztgame.com>
2021-05-13 19:37:20 +0800

    update

---
+++ src/views/temp/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="temp-container">
+    <h2>临时功能测试页</h2>
+    <div class="title">icon 使用</div>
+    <div class="panel">
+      <span class=""></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.temp-container {
+  padding: 10px;
+  .title {
+    background: #2e2868;
+  }
+  .panel {
+    padding: 20px 0;
+  }
+}
+</style>
